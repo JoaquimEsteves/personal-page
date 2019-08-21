a304abc51747446ae15b3059d12d287cc5c0314b
ESTEVES Joaquim Ext DTSI/DERS <XBXD5499@orange.com>
2019-08-21 17:34:06 +0200

    style changes to the about section

--- src/containers/about/about.tsx
+++ src/containers/about/about.tsx
@@ -29,7 +29,7 @@ export const exampleSkills: aboutProps = {
 const Skill = (skill: skill) => {
     const data_with = (skill.proficiency / 5) * 100;
     return (
-        <div key={skill.key} className="skill-item mt-25">
+        <div key={skill.key} className="skill-item">
             <div className="skill-header">
                 <div className="row">
                     <div className="col-lg-6">
@@ -65,11 +65,11 @@ export class About extends React.PureComponent<aboutProps> {
             return Skill({ ...skillProps, key: index });
         });
         return (
-            <section id="about" className="about-area pt-125 pb-130">
+            <section id="about" className="about-area">
                 <div className="container">
                     <div className="row justify-content-center">
                         <div className="col-lg-8">
-                            <div className="section-title text-center">
+                            <div className="section-title box text-center">
                                 <h2 className="title">Texto De Introducao</h2>
                                 <p>Nunc id dui at sapien faucibus fermentum ut vel diam. Nullam tempus, nunc id efficitur
                             sagittis, urna est ultricies eros, ac porta sem turpis quis leo.</p>
@@ -78,12 +78,12 @@ export class About extends React.PureComponent<aboutProps> {
                     </div>
                     <div className="row">
                         <div className="col-lg-6">
-                            <div className="about-content mt-50">
+                            <div className="about-content">
                                 <h5 className="about-title">Mais Texto</h5>
                                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
                                     labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
                             laboris nisi ut aliquip ex ea commodo consequat.</p>
-                                <ul className="clearfix">
+                                <ul>
                                     <li>
                                         <div className="single-info d-flex align-items-center">
                                             <div className="info-icon">
@@ -127,8 +127,8 @@ export class About extends React.PureComponent<aboutProps> {
                                 </ul>
                             </div>
                         </div>
-                        <div className="col-xl-5 offset-xl-1 col-lg-6">
-                            <div className="about-skills pt-25">
+                        <div className="col-lg-6">
+                            <div className="about-skills">
                                 {skillArray}
                             </div>
                         </div>
