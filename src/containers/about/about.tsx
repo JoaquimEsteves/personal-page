6df3ea03813b307e52176aeb132d62714b33fd61
ESTEVES Joaquim Ext DTSI/DERS <XBXD5499@orange.com>
2019-08-28 12:52:39 +0200

    Completed the about Section

--- src/containers/about/about.tsx
+++ src/containers/about/about.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './about.scss';
 import $ from 'jquery';
 
 export type skill = {
-    name: string,
+    name: string | JSX.Element,
     proficiency: number,
     key?: number;
 }
@@ -15,14 +15,17 @@ export type aboutProps = {
 export const exampleSkills: aboutProps = {
     skills: [
         { name: 'Python', proficiency: 5 },
-        { name: 'ESCMAScript Family', proficiency: 4 },
-        { name: 'C++', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
-        { name: 'etc', proficiency: 3 },
+        { name: 'Javascript & Family', proficiency: 4 },
+        { name: 'PHP', proficiency: 4 },
+        { name: 'SQL', proficiency: 3 },
+        {
+            name:
+                <Fragment>
+                    Java <br /><small>(It's been a long time since I've touched these)</small>
+                </Fragment>,
+            proficiency: 2
+        },
+        { name: 'C++', proficiency: 2 },
     ]
 }
 
@@ -53,9 +56,8 @@ const AboutIntro = () => (
     <div className="row justify-content-center">
         <div className="col-lg-8">
             <div className="section-title box text-center">
-                <h2 className="title">Texto De Introducao</h2>
-                <p>Nunc id dui at sapien faucibus fermentum ut vel diam. Nullam tempus, nunc id efficitur
-                            sagittis, urna est ultricies eros, ac porta sem turpis quis leo.
+                <h2 className="title">About Me</h2>
+                <p> Hello there, my name is Joaquim Esteves and this is my little corner of the internet.
                 </p>
             </div>
         </div>
@@ -97,37 +99,120 @@ export class About extends React.PureComponent<aboutProps> {
                     <div className="row">
                         <div className="col-lg-6">
                             <div className="about-content">
-                                <h5 className="about-title">Mais Texto</h5>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                                    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                            laboris nisi ut aliquip ex ea commodo consequat.</p>
+                                <h5 className="about-title">Overview</h5>
+                                <p>
+                                    I'm a Portuguese Software Engineer, I like long walks on the beach, travelling
+                                    and programming.
+                                    <br />
+                                    <small>(Being serious now, given that this isn't a
+                                        dating website profile)</small>
+                                    <br />
+                                    On my right is an overview of the
+                                    programming languages I've worked with.
+                                    <br />
+                                    I've also had to deal with quite a few frameworks and language supersets,
+                                    so if you've got a Flask/React/Etc project don't hesitate to ask me for my experience!
+
+                                </p>
                                 <ul>
-                                    {Info(
-                                        'Birth Date:',
-                                        '01/09/39',
-                                        'lni-calendar'
-                                    )}
-                                    {Info(
-                                        'Email:',
-                                        '???????',
-                                        'lni-envelope'
-                                    )}
-                                    {Info(
-                                        'Phone Number',
-                                        '???????',
-                                        'lni-phone-handset'
-                                    )}
-                                    {Info(
-                                        '',
-                                        'Currently in Lyon - France',
-                                        'lni-map-marker'
-                                    )}
+                                    {Info('Birth Date:', '01/09/39', 'lni-calendar')}
+                                    {Info('Email:', '???????', 'lni-envelope')}
+                                    {Info('Phone Number', '???????', 'lni-phone-handset')}
+                                    {Info('', 'Currently in Lyon - France', 'lni-map-marker')}
                                 </ul>
+                                <br />
+
                             </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="about-skills">
                                 {skillArray}
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="row justify-content-center">
+                        <div className="col-md-6">
+                            <div className="about-content">
+                                <h5 className="about-title">Stuff I'm not good at, but would love to learn!</h5>
+                                <ul>
+                                    <li className="mt-2">
+                                        Data Science
+                                        <p>
+                                            As a self admited Python nerd and zealot I'd love to sink my teeth into
+                                            this field!
+                                        </p>
+                                    </li>
+                                    <li className="mt-4">
+                                        Machine Learning
+                                        <p>
+                                            Have you <b>seen</b> the stuff people are up to nowadays with
+                                            machine learning? It's like magic, of course I want to learn!
+                                        </p>
+                                    </li>
+                                    <li className="mt-4">
+                                        Various FrontEnd Frameworks
+                                        <p>
+                                            I've mostly worked as a backend developer, with this very site being the
+                                            first project I've ever completed using a javascript library that's more
+                                            recent than jQuery.
+                                            However, I've  got a huge interest in this field :)
+                                            <br />
+                                            The code for this page is freely available for snooping    
+                                            <a
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                href="https://github.com/JoaquimEsteves/personal-page"> here
+                                                </a>
+                                        </p>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div className="col-md-6">
+                            <div className="about-content">
+                                <h5 className="about-title">Real World Languages</h5>
+                                <ul>
+                                    <li>
+                                        Portuguese
+                                        <p>
+                                            It's my mother tongue, and it's still my favourite language.
+                                            Although
+                                        </p>
+                                    </li>
+                                    <li>
+                                        Spanish
+                                        <p>
+                                            All Portuguese come with the Spanish language package by default.
+                                            Many of my friends throughout my life as an emigrant have been Spaniards,
+                                            and as such I've developed a not-bad accent.
+                                            <br/>
+                                            Just don't ask me to write in Spanish, It's basically Portuguese with 
+                                            gramatical mistakes! I can't for the life of me write it
+                                        </p>
+                                    </li>
+                                    <li>
+                                        English
+                                        <p>
+                                            I lived two years in Plymouth UK and have considered myself quite fluent
+                                             for many years.
+                                             <br/> I've even developed a killer impression of the accent,
+                                              you should ask me about it
+                                        </p>
+                                    </li>
+                                    <li>
+                                        French
+                                        <p>
+                                            I've been living in France for the past year and half,
+                                             having worked for the past six months entirely in French; although it's
+                                            by far my weakest language.
+                                            In classical immigrant fashion, the number of words I know is in the dozens,
+                                            and I get stumped when to native speakers talk to each other
+                                            and I get stuck in the middle, mais je parviens.
+                                        </p>
+                                    </li>
+                                </ul>
                             </div>
                         </div>
                     </div>
