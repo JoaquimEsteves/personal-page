eb40a686f195e9b59f4022dcc782775e3821c298
joaquimbve@hotmail.com <joaquimbve@hotmail.com>
2019-08-29 00:34:59 +0200

    Removed comments

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,31 +1,24 @@
 import React from 'react'
 import './header.scss';
-// import picture_of_me from '../../img/me_512.png';
 
 const Header = () => {
     return (
         <header id="home" className="header-area">
             <div className="navigation fixed-top">
-                {/* <div className="container"> */}
-                    {/* <div className="row"> */}
-                        {/* <div className="col-lg-12"> */}
-                            <nav className="navbar-expand-lg">
-                                {/* ADD LOGO HERE YO <a className="navbar-brand" href="index.html">
+
+                <nav className="navbar-expand-lg">
+                    {/* ADD LOGO HERE YO <a className="navbar-brand" href="index.html">
                                     <img src="" alt="Logo aqui" />
                                 </a> */}
 
-                                {/* <div> */}
-                                    <ul className="navbar-nav">
-                                        <li><a href="#home">Home</a></li>
-                                        <li><a href="#about">About</a></li>
-                                        <li><a href="#work">Experience</a></li>
-                                        <li><a href="#contact">Contact</a></li>
-                                    </ul>
-                                {/* </div> */}
-                            </nav>
-                        {/* </div> */}
-                    {/* </div> */}
-                {/* </div> */}
+                    <ul className="navbar-nav">
+                        <li><a href="#home">Home</a></li>
+                        <li><a href="#about">About</a></li>
+                        <li><a href="#work">Experience</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </nav>
+
             </div>
 
             <div className="header-content d-flex align-items-center">
@@ -36,7 +29,7 @@ const Header = () => {
                             <p>Computer Software Engineer</p>
                         </div>
                         <div className="col-6 offset-xl-1 mt-10">
-                            <div className="image_of_me" title="me, looking all handsome"/>
+                            <div className="image_of_me" title="me, looking all handsome" />
                         </div>
                     </div>
                 </div>
