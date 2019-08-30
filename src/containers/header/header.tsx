7a99cf1f464851b702e011f8ba18727c7267c597
ESTEVES Joaquim Ext DTSI/DERS <XBXD5499@orange.com>
2019-08-30 16:01:17 +0200

    React Hooks & next-gen images
    Project now uses webp images for browsers that support them!
    
    We accomplish this by using React Hooks on our header

--- src/containers/header/header.tsx
+++ src/containers/header/header.tsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './header.scss';
+import $ from 'jquery';
+
+const hasWebP = (function () {
+    // some small (2x1 px) test image
+    const image = "data:image/webp;base64,UklGRjIAAABXRUJQVlA4ICYAAACyAgCdASoCAAEALmk0mk0iIiIiIgBoSygABc6zbAAA/v56QAAAAA==";
+
+    return function () {
+        var deferred = $.Deferred();
+
+        $("<img>").on("load", function () {
+            let self = this as HTMLImageElement;
+            if (self.width === 2 && self.height === 1) {
+                deferred.resolve();
+            } else {
+                deferred.reject();
+            }
+        }).on("error", function () {
+            deferred.reject();
+        }).attr("src", image);
+
+        return deferred.promise();
+    }
+})();
 
 const Header = () => {
+    const [useWb, setUseWb] = useState(true)
+    useEffect(() => {
+        hasWebP().then(() => (setUseWb(true)), () => (setUseWb(false)));
+        
+    });
+    let image_class = "image_of_me_webp";;
+    if (useWb === false) {
+        image_class = 'image_of_me_no_webp'
+    }
     return (
         <header id="home" className="header-area">
             <div className="navigation fixed-top">
@@ -29,12 +61,12 @@ const Header = () => {
                             <p>Computer Software Engineer</p>
                         </div>
                         <div className="col-xl-5 col-lg-6">
-                            <div className="image_of_me" title="me, looking all handsome" />
+                            <div className={image_class} title="me, looking all handsome" />
                         </div>
                     </div>
                 </div>
             </div>
-        </header >
+        </header>
     );
 }
 
